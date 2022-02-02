c12a6b64dea4852eec9cfe16f8cb377ccd85f2de
Bala Barnabas Simon <barnabassimon486@gmail.com>
2022-02-02 13:38:44 +0300

    MVC design pattern implemented.

---
+++ Clima-Weather/Controller/WeatherViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Clima-Weather
+//
+//  Created by Barnabas Bala on 01.02.2022.
+//
+
+import UIKit
+import CoreLocation
+
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var conditionImageview: UIImageView!
+    @IBOutlet weak var temperatureLable: UILabel!
+    @IBOutlet weak var cityLable: UILabel!
+    
+    
+        override func viewDidLoad() {
+        super.viewDidLoad()
+        
+            searchTextField.delegate = self
+    }
+
+
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        return true
+        
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type a city name"
+            return false
+        }
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //Use searchTextField.text to get the weather for that city
+        searchTextField.text = ""
+    }
+    
+}
+
+
---
+++ Clima-Weather/Model/WeatherData.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherData.swift
+//  Clima-Weather
+//
+//  Created by Barnabas Bala on 01.02.2022.
+//
+
+import Foundation
